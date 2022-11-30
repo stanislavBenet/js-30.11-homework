c63a61fbcb721147c658ca9beb6a846ba74cfe78
stanislavBenet <stanislav.benet@gmail.com>
2022-11-30 20:12:22 +0200

    Add homework

--- js/index.js
+++ js/index.js
@@ -35,7 +35,7 @@ class RangeValidator {
     return this._from;
   }
   set from(from) {
-    if (from < this._to) {
+    if (from > this._to) {
       throw new RangeError("From must be less than to");
     }
     if (typeof from !== "number") {
@@ -44,33 +44,27 @@ class RangeValidator {
     this._from = from;
   }
 
-  getRange() {
+  get range() {
     let rangeArray = [this._from, this._to];
     return rangeArray;
-  }
-  get num() {
-    return this._num;
-  }
-  set num(num) {
-    if (typeof num !== "number") {
-      throw new TypeError("Value is not a number");
-    }
-    this._num = num;
   }
   /**
    *
    * @param {number} num
    */
   validate(num) {
-    return num>= this._from && num <= this._to;
+    if (typeof num !== "number") {
+      throw new TypeError("Value must be a number");
+    }
+    return num >= this._from && num <= this._to;
   }
 }
 
 try {
-  const value = new RangeValidator();
+  const value = new RangeValidator(10,1);
   console.log(value);
-  console.log(value.getRange());
-  console.log(value.validate());
+  console.log(value.range);
+  console.log(value.validate(2));
 } catch (error) {
   console.log(error);
 }
